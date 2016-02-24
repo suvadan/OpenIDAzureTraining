d69c21dbd84af7b961d163373c9345a03f1c98de
suvadan <suvadans@hexaware.com>
2016-02-24 07:24:36 +0000

    ok

--- OpenIdExample/Controllers/HomeController.cs
+++ OpenIdExample/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace OpenIdExample.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
